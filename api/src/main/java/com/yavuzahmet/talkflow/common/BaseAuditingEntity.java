ebb0c5ec6ebc5d15c1e40462c98a98a7fab4a189
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-05-04 21:54:57 +0300

    feat(auditing): create BaseAuditingEntity for automatic timestamp management

---
+++ api/src/main/java/com/yavuzahmet/talkflow/common/BaseAuditingEntity.java
@@ -0,0 +1,35 @@
+package com.yavuzahmet.talkflow.common;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+@SuperBuilder
+@EntityListeners(AuditingEntityListener.class)
+public class BaseAuditingEntity {
+
+    @CreatedDate
+    @Column(name = "created_date", nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(name = "last_modified_date", insertable = false)
+    private LocalDateTime lastModifiedDate;
+
+}
