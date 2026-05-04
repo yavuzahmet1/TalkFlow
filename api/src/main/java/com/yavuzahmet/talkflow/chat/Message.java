16419d0db41ea128e3f596ece5de3d9b157c6369
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-05-04 21:54:28 +0300

    feat(chat): create Message entity with fields for content, state, type, and relationships

---
+++ api/src/main/java/com/yavuzahmet/talkflow/chat/Message.java
@@ -0,0 +1,53 @@
+package com.yavuzahmet.talkflow.chat;
+
+import com.yavuzahmet.talkflow.common.BaseAuditingEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "messages")
+public class Message extends BaseAuditingEntity {
+
+    @Id
+    @SequenceGenerator(name = "message_seq", sequenceName = "message_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "message_seq")
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private MessageState state;
+
+    @Enumerated(EnumType.STRING)
+    private MessageType type;
+
+    @ManyToOne
+    @JoinColumn(name = "chat_id", nullable = false)
+    private Chat chat;
+
+    @Column(name = "sender_id", nullable = false)
+    private String senderId;
+
+    @Column(name = "receiver_id", nullable = false)
+    private String receiverId;
+
+}
