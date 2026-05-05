ff93ccf473e711e1591f5a629a287e27ed7bf06e
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-05-05 19:09:47 +0300

    feat(user): add UserConstants class for user query constants

---
+++ api/src/main/java/com/yavuzahmet/talkflow/user/UserConstants.java
@@ -0,0 +1,12 @@
+package com.yavuzahmet.talkflow.user;
+
+public class UserConstants {
+
+    public static final String FIND_USER_BY_EMAIL = "Users.findUserByEmail";
+    public static final String FIND_ALL_USERS_EXCEPT_SELF = "Users.findAllUsersExceptSelf";
+    public static final String FIND_USER_BY_PUBLIC_ID = "Users.findUserByPublicId";
+
+    private UserConstants() {
+        // Private constructor to prevent instantiation
+    }
+}
