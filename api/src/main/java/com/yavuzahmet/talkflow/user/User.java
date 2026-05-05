d2d14e21579f4fc352a2202d58e646e27f526788
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-05-05 19:09:36 +0300

    feat(user): add NamedQueries for user retrieval by email and public ID

--- api/src/main/java/com/yavuzahmet/talkflow/user/User.java
+++ api/src/main/java/com/yavuzahmet/talkflow/user/User.java
@@ -8,6 +8,7 @@ import com.yavuzahmet.talkflow.common.BaseAuditingEntity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
@@ -22,6 +23,9 @@ import lombok.Setter;
 @NoArgsConstructor
 @Entity
 @Table(name = "users")
+@NamedQuery(name = UserConstants.FIND_USER_BY_EMAIL, query = "SELECT u FROM User u WHERE u.email = :email")
+@NamedQuery(name = UserConstants.FIND_ALL_USERS_EXCEPT_SELF, query = "SELECT u FROM User u WHERE u.id != :publicId")
+@NamedQuery(name = UserConstants.FIND_USER_BY_PUBLIC_ID, query = "SELECT u FROM User u WHERE u.publicId = :publicId")
 public class User extends BaseAuditingEntity {
 
     private static final int ONLINE_TIMEOUT_MINUTES = 5;
