7c65da2073fa4c68d9f33d86e3f1e4be19e3fa29
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-05-05 19:09:22 +0300

    feat(chat): add ChatConstants class for chat query constants

---
+++ api/src/main/java/com/yavuzahmet/talkflow/chat/ChatConstants.java
@@ -0,0 +1,12 @@
+package com.yavuzahmet.talkflow.chat;
+
+public class ChatConstants {
+
+    public static final String FIND_CHAT_BY_SENDER_ID = "Chat.findChatBySenderId";
+    public static final String FIND_CHAT_BY_RECIPIENT_ID = "Chat.findChatByRecipientId";
+
+    private ChatConstants() {
+
+    }
+
+}
