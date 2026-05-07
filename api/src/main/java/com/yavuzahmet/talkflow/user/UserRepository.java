27af1f8a937147720c071a8f65b8b0651ad22ca0
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-05-07 21:26:37 +0300

    feat(user): add findByEmail method to UserRepository for user retrieval by email

--- api/src/main/java/com/yavuzahmet/talkflow/user/UserRepository.java
+++ api/src/main/java/com/yavuzahmet/talkflow/user/UserRepository.java
@@ -1,7 +1,14 @@
 package com.yavuzahmet.talkflow.user;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, String> {
+
+    @Query(name = UserConstants.FIND_USER_BY_EMAIL)
+    Optional<User> findByEmail(@Param("email") String userEmail);
 
 }
