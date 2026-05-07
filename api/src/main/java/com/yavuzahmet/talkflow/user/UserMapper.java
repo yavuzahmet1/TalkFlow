2e73521a1c0a0fa0f202f6b64e6e770076b0646c
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-05-07 21:26:26 +0300

    feat(user): implement fromTokenAttributes method in UserMapper for user data mapping

--- api/src/main/java/com/yavuzahmet/talkflow/user/UserMapper.java
+++ api/src/main/java/com/yavuzahmet/talkflow/user/UserMapper.java
@@ -1,8 +1,35 @@
 package com.yavuzahmet.talkflow.user;
 
+import java.time.LocalDateTime;
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserMapper {
+
+    public User fromTokenAttributes(Map<String, Object> attributes) {
+        User user = new User();
+
+        if (attributes.containsKey("sub")) {
+            user.setId(attributes.get("sub").toString());
+        }
+
+        if (attributes.containsKey("given_name")) {
+            user.setFirstName(attributes.get("given_name").toString());
+        }
+
+        if (attributes.containsKey("family_name")) {
+            user.setLastName(attributes.get("family_name").toString());
+        }
+
+        if (attributes.containsKey("email")) {
+            user.setEmail(attributes.get("email").toString());
+        }
+
+        user.setLastSeen(LocalDateTime.now());
+
+        return user;
+    }
 
 }
