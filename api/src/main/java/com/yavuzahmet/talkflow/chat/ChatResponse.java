d0c2a7ab265d43ef6db924fdd1ff58ba560007b5
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-05-09 22:33:53 +0300

    feat(chat): enhance ChatResponse class with additional fields for chat details

--- api/src/main/java/com/yavuzahmet/talkflow/chat/ChatResponse.java
+++ api/src/main/java/com/yavuzahmet/talkflow/chat/ChatResponse.java
@@ -1,5 +1,24 @@
 package com.yavuzahmet.talkflow.chat;
 
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ChatResponse {
+
+    private String id;
+    private String name;
+    private Long unreadCount;
+    private String lastMessage;
+    private LocalDateTime lastMessageTime;
 
 }
