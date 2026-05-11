366b133bf11a2b3796c8d90f7d802b21d44fd4cf
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-05-11 21:47:38 +0300

    feat(chat): update ChatRepository to include findChatsBySenderId method with custom query

--- api/src/main/java/com/yavuzahmet/talkflow/chat/ChatRepository.java
+++ api/src/main/java/com/yavuzahmet/talkflow/chat/ChatRepository.java
@@ -3,9 +3,11 @@ package com.yavuzahmet.talkflow.chat;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ChatRepository extends JpaRepository<Chat, String> {
 
-    List<ChatResponse> findByRecieverId(String recieverId);
-
+    @Query(name = ChatConstants.FIND_CHAT_BY_SENDER_ID)
+    List<Chat> findChatsBySenderId(@Param("senderId") String userId);
 }
