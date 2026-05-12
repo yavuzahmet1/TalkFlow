180af05963841db1488088d91b37d65cb240dca8
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-05-12 22:06:25 +0300

    feat(chat): refactor ChatService to include UserRepository and enhance createChat method

--- api/src/main/java/com/yavuzahmet/talkflow/chat/ChatService.java
+++ api/src/main/java/com/yavuzahmet/talkflow/chat/ChatService.java
@@ -1,7 +1,11 @@
 package com.yavuzahmet.talkflow.chat;
 
 import java.util.List;
+import java.util.Optional;
 
+import com.yavuzahmet.talkflow.user.User;
+import com.yavuzahmet.talkflow.user.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,10 +14,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class ChatService {
 
     private final ChatRepository chatRepository;
+    private final UserRepository userRepository;
     private final ChatMapper mapper;
 
-    public ChatService(ChatRepository chatRepository, ChatMapper mapper) {
+    public ChatService(ChatRepository chatRepository, UserRepository userRepository,ChatMapper mapper) {
         this.chatRepository = chatRepository;
+        this.userRepository = userRepository;
         this.mapper = mapper;
     }
 
@@ -25,5 +31,24 @@ public class ChatService {
                 .stream()
                 .map(chat -> mapper.toChatResponse(chat, userId))
                 .toList();
+    }
+
+    public String createChat(String senderId, String receiverId) {
+
+        Optional<Chat> exitingChat=chatRepository.findChatsByReceiverAndSender(senderId,receiverId);
+
+        if (exitingChat.isPresent()) {
+            return exitingChat.get().getId();
+        }
+
+        User sender=userRepository.findByPublicId(senderId).orElseThrow(()->new EntityNotFoundException("User not found : "+senderId));
+        User receiver=userRepository.findByPublicId(senderId).orElseThrow(()->new EntityNotFoundException("User not found : "+receiverId));
+
+        Chat chat=new Chat();
+        chat.setSender(sender);
+        chat.setRecipient(receiver);
+
+        Chat savedChat=chatRepository.save(chat);
+        return savedChat.getId();
     }
 }
