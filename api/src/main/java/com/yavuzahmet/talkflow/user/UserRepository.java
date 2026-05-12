53e214efc62e4ea57a87d4b7f4d55a90d07f8491
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-05-12 22:06:36 +0300

    feat(user): add findByPublicId method to UserRepository

--- api/src/main/java/com/yavuzahmet/talkflow/user/UserRepository.java
+++ api/src/main/java/com/yavuzahmet/talkflow/user/UserRepository.java
@@ -11,4 +11,6 @@ public interface UserRepository extends JpaRepository<User, String> {
     @Query(name = UserConstants.FIND_USER_BY_EMAIL)
     Optional<User> findByEmail(@Param("email") String userEmail);
 
+    @Query(name = UserConstants.FIND_USER_BY_PUBLIC_ID)
+    Optional<User> findByPublicId(String publicId);
 }
