a1291854d0803de4c1a47495cf8585d4e2ff04cd
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-05-13 20:57:10 +0300

    feat(common): add StringResponse class for standardized string responses

---
+++ api/src/main/java/com/yavuzahmet/talkflow/common/StringResponse.java
@@ -0,0 +1,13 @@
+package com.yavuzahmet.talkflow.common;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class StringResponse {
+private String response;
+
+}
