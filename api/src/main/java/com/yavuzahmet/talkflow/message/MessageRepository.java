228e7d8dd90725e6b86cc31d8643bfee6a262057
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-05-15 21:56:28 +0300

    feat(message): add custom queries for finding messages by chat ID and setting messages to seen

--- api/src/main/java/com/yavuzahmet/talkflow/message/MessageRepository.java
+++ api/src/main/java/com/yavuzahmet/talkflow/message/MessageRepository.java
@@ -1,6 +1,19 @@
 package com.yavuzahmet.talkflow.message;
 
+import com.yavuzahmet.talkflow.chat.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
+
+    @Query(name=MessageConstants.FIND_MESSAGES_BY_CHAT_ID)
+    List<Message> findMessagesByChatId(String chatId);
+
+    @Query(name = MessageConstants.SET_MESSAGES_TO_SEEN_BY_CHAT)
+    @Modifying
+    void setMessagesToSeenByChatId(@Param("chatId")Chat chatId, @Param("newState")MessageState state);
 }
