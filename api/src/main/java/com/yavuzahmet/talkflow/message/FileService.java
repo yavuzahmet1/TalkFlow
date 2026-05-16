c512b3fd2c89b94a907b0408f531706fc5c8dc56
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-05-16 22:39:45 +0300

    feat(file): enhance FileService with file upload functionality and logging

--- api/src/main/java/com/yavuzahmet/talkflow/message/FileService.java
+++ api/src/main/java/com/yavuzahmet/talkflow/message/FileService.java
@@ -1,11 +1,82 @@
 package com.yavuzahmet.talkflow.message;
 
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static java.io.File.separator;
+
 @Service
+@Slf4j
 public class FileService {
-    public String saveFile(MultipartFile file, String senderId) {
+
+    @Value("${application.file.uploads.media-output-path}")
+    private String fileUploadPath;
+
+    public String saveFile(@NonNull MultipartFile sourceFile, @NonNull String senderId) {
+        final String fileUploadSubPath = "users" + separator + senderId;
+        return uploadFile(sourceFile,fileUploadSubPath);
+
+    }
+
+    public String uploadFile(@NonNull MultipartFile sourceFile, @NonNull String fileUploadSubPath) {
+        final String finalUploadPath = fileUploadPath + separator + fileUploadSubPath;
+        File targetFolder = new File(finalUploadPath);
+
+        if (!targetFolder.exists()) {
+            boolean folderCreated = targetFolder.mkdirs();
+            if (!folderCreated) {
+                log.error("Could not create the directory {}.", targetFolder);
+                return null;
+            }
+        }
+
+        final  String fileExtension=getFileExtension(sourceFile.getOriginalFilename());
+
+        String targetFilePath =finalUploadPath+separator+System.currentTimeMillis()+"."+fileExtension;
+
+        Path targetPath = Paths.get(targetFilePath);
+
+        try {
+
+            Files.write(targetPath,sourceFile.getBytes());
+
+            log.info("File uploaded successfully.{}",targetPath);
+
+            return targetFilePath;
+
+        }catch(IOException e){
+
+            log.error("Could not save file. {}",targetPath,e);
+
+        }
+
         return null;
+
+    }
+
+    private String getFileExtension(String fileName) {
+
+        if (fileName == null||fileName.isEmpty()) {
+
+            return "";
+        }
+
+        int lastDotIndex = fileName.lastIndexOf('.');
+
+        if (lastDotIndex == -1) {
+
+            return "";
+        }
+
+        return fileName.substring(lastDotIndex + 1).toLowerCase();
     }
 }
