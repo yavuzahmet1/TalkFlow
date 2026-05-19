b0b53242e8a5bb188510243c8b84a3e1a0687f1c
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-05-19 23:01:46 +0300

    feat(user): implement UserService for user management functionality

---
+++ api/src/main/java/com/yavuzahmet/talkflow/user/UserService.java
@@ -0,0 +1,25 @@
+package com.yavuzahmet.talkflow.user;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserService(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
+    public List<UserResponse> getAllUserExceptSelf(Authentication connectedUser){
+        return userRepository.findAllUsersExceptSelf(connectedUser.getName())
+                .stream()
+                .map(userMapper::toUserResponse)
+                .toList();
+    }
+}
