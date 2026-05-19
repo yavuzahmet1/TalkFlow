ddfe329c21af3eb517113733beec919be4e73483
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-05-19 23:01:38 +0300

    feat(user): create UserResponse class for user data representation

---
+++ api/src/main/java/com/yavuzahmet/talkflow/user/UserResponse.java
@@ -0,0 +1,21 @@
+package com.yavuzahmet.talkflow.user;
+
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserResponse {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDateTime lastSeen;
+    private boolean isOnline;
+}
