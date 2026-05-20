323b2f64b1ab92ad654c528b928768663466eaf9
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-05-20 20:48:05 +0300

    feat(user): implement UserController for user retrieval functionality

---
+++ api/src/main/java/com/yavuzahmet/talkflow/user/UserController.java
@@ -0,0 +1,26 @@
+package com.yavuzahmet.talkflow.user;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> getAllUsers(Authentication authentication) {
+
+        return ResponseEntity.ok(userService.getAllUserExceptSelf(authentication));
+
+    }
+}
